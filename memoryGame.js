04cc4481439338e217e7dac211a9b8372c9af0e1
endiesworld <endiesworld@gmail.com>
2019-06-26 20:19:59 +0100

    Trying to Update Remot Repot

--- memoryGame.js
+++ memoryGame.js
@@ -1,110 +1,86 @@
 window.onload = function (){
-
   let firstClick = false ;
   let box = document.querySelectorAll(".container");
-
   for(let x = 0 ; x< box.length ; x++){
   box[x].addEventListener("click", processClick) ;
-//  box[x].removeEventListener("click", processClick) ;
   }
 
   let imageArray = [] ;
   let imageValue = [] ;
   let documentNode = [] ;
   let openedNode = [] ;
-  let proceed = false ;
-  let treat = true ;
+  const gameState = {proceed: "false" , treat: "true" , angle: 180  } ;
   generateImage() ;
   loadImage() ;
   let trial = 0 ;
-  let angle = 180 ;
   let showTime = document.getElementById("time") ;
   let sec = 0 ;
-  let realSec = 0 ;
   let min = 0 ;
-  let realMin = 0 ;
   let hour = 0 ;
-  let realHour = 0 ;
-  let realTime = 0 ;
-  var timming ;
+  let timming ;
+  let time = 0 ;
   let numberOfClicks = 0 ;
-  function timerStop(){
+  var timerStop = () => {
   initializeTimer() ;
   clearInterval(timming) ;
 }
 
- function timerStart(){
-  timming = setInterval(second , 1000);
+ var timerStart = () => {
+  timming = setInterval(increaseTime , 1000);
 }
 
-  function second(){
-   sec++ ;
-   if (sec > 59){
-     sec = 0 ;
-     increaseMin() ;
-   }
-   realSec = sec.toString() ;
-   realMin = min.toString() ;
-   realHour = hour.toString() ;
-   if (realSec.length === 1)
-       realSec =  "0"+sec ;
-  if (realMin.length === 1)
-      realMin =  "0"+ min ;
-  if (realHour.length === 1)
-      realHour =  "0"+ hour ;
-   realTime = realHour + ":" +realMin + ":" + realSec ;
-   showTime.innerHTML = realTime;
+  var increaseTime = () => {
+     time++ ;
+     sec = time % 60 ;
+     min = Math.floor(time / 60) ;
+     hour = Math.floor(min / 60) ;
+    writeTime() ;
  }
 
-  function increaseMin() {
-   min++ ;
-   if (min > 59){
-      min = 0 ;
-      increaseHour() ;
-     }
- }
+var writeTime = () => {
+let sec2 = sec.toString();
+let min2 = min.toString();
+let hour2 = hour.toString();
+if (sec2.length === 1)
+      sec2 =  `0${sec2}` ;
+if (min2.length === 1)
+      min2 =  `0${min2}` ;
+if (hour2.length === 1)
+      hour2 =  `0${hour2}` ;
+showTime.innerHTML = `${hour2}:${min2}:${sec2}`;
+}
 
-  function increaseHour() {
-   hour++ ;
-  }
-
-  function initializeTimer(){
+  var initializeTimer = () => {
    sec = 0 ;
-   realSec = 0 ;
    min = 0 ;
-   realMin = 0 ;
    hour = 0 ;
-   realHour = 0 ;
-   realTime = 0 ;
 }
 
-function processClick(){
-  console.log(" Document clicked on: " + event.target.id) ;
-  if (treat){
+ function processClick(){
+  if (gameState.treat){
   trial++ ;
-  console.log("trial number is  " + trial) ;
   firstClickCheck() ;
   rotateTile() ;
-  proceed =  storeDocument() ;
-  if (proceed){
+  gameState.proceed =  storeDocument() ;
+  if (gameState.proceed){
   extractImageValue() ;
     }
   }
 }
 
-function rotateTile() {
-  let rotate = "rotateY(" + angle + "deg" + ")" ;
+var rotateTile = () => {
+  let rotate = "rotateY(" + gameState.angle + "deg" + ")" ;
   event.target.parentNode.style.transform = rotate;
 }
 
-function firstClickCheck(){
+var firstClickCheck = () =>{
    if(firstClick == false){
-     timerStart();
+     timerStart() ;
      firstClick = true ;
    }
 }
 
- function storeDocument() {
+ var storeDocument = () => {
    let node = event.target.parentNode ;
    let toProceed = false ;
    let internalControl = true ;
@@ -121,14 +97,12 @@ function firstClickCheck(){
    else if (internalControl && trial === 2 && ( documentNode[0] != node ) ){
      documentNode[1] = node ;
      toProceed = true ;
-
          }
    else if ( internalControl && trial === 3 && ( documentNode[1] != node ) && ( documentNode[0] != node ) ){
       documentNode[2] = node ;
        toProceed = true ;
-       treat = false ;
-       console.log("the value for treat is : " + treat) ;
-            }
+       gameState.treat = false ;
+  }
   else {
    trial-- ;
    toProceed = false ;
@@ -136,7 +110,8 @@ function firstClickCheck(){
  return toProceed ;
  }
 
-function extractImageValue(){
+
+var extractImageValue = () =>{
    let y ;
    let z ;
    y = event.target.id;
@@ -146,7 +121,7 @@ function extractImageValue(){
    storeImageValue(y) ;
 }
 
-function storeImageValue(value ){
+var storeImageValue = (value ) => {
   if (trial  === 1){
     imageValue[0] = value ;
     }
@@ -159,20 +134,18 @@ function storeImageValue(value ){
    }
 }
 
-function compareImage(){
-
+var compareImage = ()=>{
   if (imageValue[0] == imageValue[1])
     storeNode() ;
   else
     closeImage() ;
 }
 
-function storeNode(){
+var storeNode = () => {
   let thirdImage = imageValue[2] ;
   let thirdNode =  documentNode[2] ;
   trial = 1 ;
-  //
-  setTimeout( function(){ treat = true ;} , 500 ) ;
+  setTimeout( function(){ gameState.treat = true ;} , 500 ) ;
   openedNode.push(documentNode[0]) ;
   openedNode.push(documentNode[1]) ;
   imageValue[0] = thirdImage  ;
@@ -181,102 +154,71 @@ function storeNode(){
   imageValue[x] = 0 ;
   documentNode[x] = 0;
   }
-
 }
 
-function closeImage(){
+var closeImage = ()=> {
   setTimeout( function(){
   for(let x = 0 ; x<3 ; x++) {
   documentNode[x].style.transform = "rotateY(360deg)";
   documentNode[x] = 0 ;
   imageValue[x] = 0 ;
 }
-treat = true ;
+gameState.treat = true ;
 }, 1000 ) ;
   trial = 0 ;
-
 }
 
 function generateImage() {
-/**
-The below forms a new element <img height= 100, width = 100, src = ??
-let amara = new Image(100, 100) ;
-amara.src = 'amara.jpg' ;
-**/
  let name = [] ;
- name[0] = "url('austin.jpg')" ;
- name[1] = "url('amara.jpg')" ;
- name[2] = "url('brown.jpg')" ;
- name[3] = "url('chi.jpg')" ;
- name[4] = "url('chizzy.jpg')" ;
- name[5] = "url('dubem.jpg')" ;
- name[6] = "url('fav.jpg')" ;
- name[7] = "url('parents.JPG')" ;
- name[8] = "url('parents2.jpg')" ;
- name[9]  = "url('ugo.jpg')" ;
+ name[0] = "url('images/austin.jpg')" ;
+ name[1] = "url('images/amara.jpg')" ;
+ name[2] = "url('images/brown.jpg')" ;
+ name[3] = "url('images/chi.jpg')" ;
+ name[4] = "url('images/chizzy.jpg')" ;
+ name[5] = "url('images/dubem.jpg')" ;
+ name[6] = "url('images/fav.jpg')" ;
+ name[7] = "url('images/parents.JPG')" ;
+ name[8] = "url('images/parents2.jpg')" ;
+ name[9]  = "url('images/ugo.jpg')" ;
 for(x in name)
   imageArray.push(name[x]);
 }
 
-function loadImage(){
-let documentNumber ;
-let documentHolder = [] ;
-let imageNumber ;
-let imageHolder = [] ;
-let multipleImage = [] ;
-let proceedDocument = true ;
-let proceedImage = true ;
-let parent = document.getElementsByClassName('front') ;
-for (let x = 0 ; x < 20 ; x++) {
+function loadImage() {
+   let boxPosition = [] ;
+   let boxNumbers = 20 ;
+   let imageNumber = 10 ;
+   let imageHolder = [] ;
+   let multiplyImage = [] ;
+   let parent = document.getElementsByClassName('front') ;
+   boxPosition = randomWithNoRepete(boxNumbers) ;
+   imageHolder = randomWithNoRepete(imageNumber) ;
+   multiplyImage = imageHolder.concat(imageHolder) ;
+   for (let x = 0 ; x < 20 ; x++)
+      parent[boxPosition[x]].style.backgroundImage =   imageArray[multiplyImage[x]] ;
+    }
+
+  function randomWithNoRepete(size){
+    let numberHolder = [] ;
+    let  numberGenerated ;
+    let saveNumber = true ;
+    let limiter = size ;
+    size -= 1 ;
     do {
-    documentNumber = randomNumber() ;
-  for (data in documentHolder){
-     if(documentHolder[data] === documentNumber){
-        proceedDocument = false ;
-        break ;
-       }
-     else {
-      proceedDocument = true ;
-       }
+         numberGenerated  = Math.floor( Math.random() * limiter) ;
+         for (data in numberHolder){
+            if(numberHolder[data] === numberGenerated){
+               saveNumber = false ;
+               break ;
+                  }
+             }
+             if(saveNumber){
+                numberHolder[size] = numberGenerated ;
+                size-- ;
+             }
+            saveNumber = true ;
+            } while(size >= 0) ;
+     return numberHolder ;
      }
-   } while(!proceedDocument) ;
-   documentHolder[x] = documentNumber ;
-  }
-  console.log("Document numbers used: " + documentHolder) ;
- for (let x = 0 ; x < 10 ; x++) {
-   do {
-     imageNumber = randomImage() ;
-     for (data in imageHolder){
-        if(imageHolder[data] === imageNumber){
-           proceedImage = false ;
-           break ;
-          }
-        else {
-         proceedImage = true ;
-          }
-        }
-      } while(!proceedImage) ;
-    imageHolder[x] = imageNumber ;
-    multipleImage = imageHolder.concat(imageHolder) ;
-}
-console.log("Image numbers used: " + multipleImage) ;
-for (let x = 0 ; x < 20 ; x++)
-   parent[documentHolder[x]].style.backgroundImage =   imageArray[multipleImage[x]] ;
-
- }
-
- function randomNumber(){
-        let  numberGenerated = Math.floor( Math.random() * 20) ;
-        return numberGenerated ;
-  }
-
-  function randomImage(){
-      let  numberGenerated = Math.floor( Math.random() * 10) ;
-      return numberGenerated ;
-   }
-
-
-
-
 
   }
